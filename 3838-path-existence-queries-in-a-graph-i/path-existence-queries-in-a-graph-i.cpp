93dfad0defb4a609a306781c6aca1eda696f4e47
akwey <satnalikaakwey@gmail.com>
2026-07-09 15:42:18 +0530

    Time: 0 ms (100.00%) | Memory: 224.2 MB (66.03%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,58 +1,20 @@
-class DSU{
-    vector<int> parent;
-
-public:
-    DSU(int n){
-        for(int i = 0; i < n; i++){
-            parent.push_back(i);
-        }
-    }
-
-    int find(int x){
-        if(parent[x] == x){
-            return x;
-        }
-
-        return parent[x] = find(parent[x]);
-    }
-
-    void merge(int x, int y){
-        int parX = find(x);
-        int parY = find(y);
-
-        parent[parX] = parY;
-    }
-};
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-         priority_queue<pair<int, int>> pq;
-
-        for(int i = 0; i < n; i++){
-            pq.push({nums[i], i});
-        }
-
-        DSU dsu(n);
-
-        pair<int, int> temp = pq.top();
-
-        while(!pq.empty()){
-            pair<int, int> curr = pq.top();
-            pq.pop();
-
-            if(abs(temp.first - curr.first) <= maxDiff){
-                dsu.merge(temp.second, curr.second);
+        vector<int> group(n, 0);
+        int currentGroup = 0;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                currentGroup++;
             }
-
-            temp = curr;
+            group[i] = currentGroup;
         }
 
-        vector<bool> sol;
-
-        for(auto &it : queries){
-            sol.push_back(dsu.find(it[0]) == dsu.find(it[1]));
+        vector<bool> res;
+        for (auto& q : queries) {
+            int u = q[0], v = q[1];
+            res.push_back(group[u] == group[v]);
         }
-
-        return sol;
+        return res;
     }
 };
